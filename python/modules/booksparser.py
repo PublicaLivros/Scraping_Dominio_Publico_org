436e26cc4fb8b0eb1521823a67c2ca58067ffa67
Aldair Meneses <aldair.meneses@protonmail.com>
2023-04-12 13:45:30 -0300

    add regex to remove to replace symbols.

--- python/modules/booksparser.py
+++ python/modules/booksparser.py
@@ -1,15 +1,14 @@
+import re
 
 # Substitui espaços dos arquivos por "_" e coloca todos os caracteres em letra minúscula.
 def fileNameNormalize(inputString):
     # TODO: Substituir posteriormente por regex.
-    bookNormalizeText = inputString.replace(" ", "_")
+    bookNormalizeText = re.sub(u'[^a-zA-Z0-9áéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕçÇ: ]', '', inputString)
+    bookNormalizeText = bookNormalizeText.replace(" ", "_")
     bookNormalizeText = bookNormalizeText.lower()
-
     return bookNormalizeText
 
 # Substitui o link do livro para o link de download do arquivo.
 def replaceLinkToDownload(url):
     url = url.replace("DetalheObraForm.do", "DetalheObraDownload.do", 1)
     return url
-
-
